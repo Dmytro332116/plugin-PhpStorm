415140a3ff9461cf32078fadb33d449ce0809372
Dmytro Urdzik <ai_dmytro@ukrcoder.com.ua>
2026-01-28 15:16:32 +0100

    Bump compatibility and publish 0.1.2 build

---
+++ plugin_unpacked/плагін PhpStorm/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("jvm") version "1.9.24"
+    id("org.jetbrains.intellij") version "1.17.3"
+}
+
+group = "com.splitwise.component"
+version = "0.1.2"
+
+repositories {
+    mavenCentral()
+}
+
+intellij {
+    type.set("PS")
+    version.set("2023.3")
+    plugins.set(listOf())
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+tasks {
+    patchPluginXml {
+        sinceBuild.set("233")
+        untilBuild.set("253.*")
+    }
+}
